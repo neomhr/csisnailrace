fbbf22751beaff7912e61f09fa85c038acc17d10
neomhr <60556019+neomhr@users.noreply.github.com>
2020-08-17 22:39:56 +0200

    style changes, bet office modal

--- modal.js
+++ modal.js
@@ -22,3 +22,13 @@ window.onclick = function (event) {
     }
 }
 
+// Bet Amount Slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
