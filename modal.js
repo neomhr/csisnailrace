e7093b7e06c120f401683a5b3777167e18a4d663
neomhr <60556019+neomhr@users.noreply.github.com>
2020-08-19 18:57:56 +0200

    made some style changes

--- modal.js
+++ modal.js
@@ -22,13 +22,26 @@ window.onclick = function (event) {
     }
 }
 
+btn.onkeyup = function () {
+    modal.style.display = "block";
+}
+
+close.onkeyup = function () {
+    modal.style.display = "none";
+}
+
+window.onkeyup = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 // Bet Amount Slider
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+var slider = document.getElementById("betAmount");
+var output = document.getElementById("amountChosen");
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
     output.innerHTML = this.value;
 }
